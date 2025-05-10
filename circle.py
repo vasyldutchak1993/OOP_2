b2fd9b38849273e920bff23e3ec7c69173a9074a
vasyldutchak1993 <vasuldutschak@mail.ru>
2025-05-10 18:31:28 +0200

    create circle class

---
+++ circle.py
@@ -0,0 +1,31 @@
+import math
+
+from shape import Shape
+
+
+class Circle(Shape):
+
+    def __init__(self, radius, precision=4):
+        self._radius = radius
+        self._precision = precision
+
+    def area(self):
+        return round(math.pi * self._radius * self._radius,self.precision)
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, value):
+        self._radius = value
+
+    @property
+    def precision(self):
+        return self._precision
+
+    @precision.setter
+    def precision(self, value):
+        if value < 0:
+            raise ValueError("Precision must be a non-negative integer")
+        self._precision = value
