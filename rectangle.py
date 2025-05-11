bf068f90b0e4aecaaec9bedbf92d5a594e423078
vasyldutchak1993 <vasuldutschak@mail.ru>
2025-05-11 08:35:29 +0200

    add new method

--- rectangle.py
+++ rectangle.py
@@ -25,3 +25,19 @@ class Rectangle(Shape):
     @height.setter
     def height(self, height):
         self._height = height
+
+    def __float__(self):
+        return self.area()
+
+    def __int__(self):
+        return int(self.area())
+
+    def __add__(self, other):
+        if not isinstance(other, Rectangle):
+            raise TypeError("Can only add rectangle to Rectangle")
+        new_width = self._width + other._width
+        new_height = self._height + other._height
+        return Rectangle(new_width,new_height, self.precision)
+
+    def __str__(self):
+        return f"Rectangle with  {str(self._width)}  and height {str(self._height)} area is  {str(self.area())}"
